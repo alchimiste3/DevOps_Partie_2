62c720c7666fa46edb6c7e57383be2f034ad7c35
Quentin LABORDE <lq202052@etu.unice.fr>
2016-03-17 15:19:17 +0100

    chaine compile avec BEAUCOUP de probleme

--- build_all.sh
+++ build_all.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
 ## Building the .Net system
+echo ""
+echo "Copilation du server dotNet"
+echo ""
 cd dotNet
 ./compile.sh
 cd ..
 
 ## Building the j2e system
+echo ""
+echo "Copilation des composants j2e"
+echo ""
 cd j2e
+mvn -q clean package
+cd ..
+
+## Building the webService system
+echo ""
+echo "Copilation des web Services"
+echo ""
+cd webService
+mvn -q clean package
+cd ..
+
+## Building the stubs system
+echo ""
+echo "Copilation des stubs"
+echo ""
+cd stubs
 mvn clean package
 cd ..
 
-# ## Building the webService system
-# cd webService
-# mvn clean package
-# cd ..
 
-# ## Building the stubs system
-# cd stubs
-# mvn clean package
-# cd ..
-
-
-# ## Building client
-# cd client
-# mvn clean package
-# cd ..
+## Building client
+echo ""
+echo "Copilation du client"
+echo ""
+cd client
+mvn clean package
+cd ..
 
 
